f816b54ca8a153f17695cf307ca9a7f65f0baa14
Phuong Hau <hvphuong98@gmail.com>
2020-09-14 11:20:05 +0700

    add cache mechanism

--- LOL_counter_pick/lol.sh
+++ LOL_counter_pick/lol.sh
@@ -6,16 +6,34 @@ RED='\033[0;31m'
 WHITE='\033[1;37m'
 NC='\033[0m'
 
+COUNTER_SP="COUNTER"
+WEAK_AGAINST_SP="WEAK_AGAINST"
+STRONG_AGAINST_SP="STRONG_AGAINST"
+COUNTER_TIPS_SP="COUNTER_TIPS"
+
 get_weak_against () {
 	champ_name=$1
-
-	curl -s https://lolcounter.com/champions/$champ_name/weak | grep -oP "(?<=class='name'>).+(?=</div>)"
+	if [[ -f ".cache/$champ_name" ]] && [[ ! -z $(get_cache $champ_name $WEAK_AGAINST_SP) ]]; then
+		echo "There is cache"
+		get_cache $champ_name $WEAK_AGAINST_SP	
+	else
+		data=$(curl -s https://lolcounter.com/champions/$champ_name/weak | grep -oP "(?<=class='name'>).+(?=</div>)")
+		echo $data
+		create_cache $champ_name "$data" $WEAK_AGAINST_SP
+	fi
 }
 
 get_strong_against () {
 	champ_name=$1
 
-	curl -s https://lolcounter.com/champions/$champ_name/strong | grep -oP "(?<=class='name'>).+(?=</div>)"
+	if [[ -f ".cache/$champ_name" ]] && [[ ! -z $(get_cache $champ_name $STRONG_AGAINST_SP) ]]; then
+		echo "get from cache..."
+		get_cache $champ_name $STRONG_AGAINST_SP	
+	else
+		data=$(curl -s https://lolcounter.com/champions/$champ_name/strong | grep -oP "(?<=class='name'>).+(?=</div>)")
+		echo $data
+		create_cache $champ_name "$data" $STRONG_AGAINST_SP
+	fi
 }
 
 get_goes_well_with () {
@@ -26,8 +44,15 @@ get_goes_well_with () {
 
 get_general_tips () {
 	champ_name=$1
-	curl -s https://lolcounter.com/champions/$champ_name | grep -oP "(?<=class='_tip'>).+(?=\.)"
+	if [[ -f ".cache/$champ_name" ]] && [[ ! -z $(get_cache $champ_name $COUNTER_TIPS_SP) ]]; then
+		get_cache $champ_name $COUNTER_TIPS_SP	
+	else
+		data=$(curl -s https://lolcounter.com/champions/$champ_name | grep -oP "(?<=class='_tip'>).+(?=\.)")
+		echo "$data"
+		create_cache $champ_name "$data" $COUNTER_TIPS_SP
+	fi
 }
+
 print_in_one_line () {
 	echo "$@" 
 }
@@ -38,6 +63,22 @@ print_list () {
 	done <<< "$@"
 }
 
+create_cache () {
+	champ_name=$1
+	data=$2
+	section=$3
+	if [[ ! -d ".cache" ]]; then
+		mkdir ".cache"
+	fi
+
+	echo -e "$section\n$data\nEND_$section" >> ".cache/$champ_name"
+}
+
+get_cache () {
+	champ_name=$1
+	section=$2
+        cat ".cache/$champ_name" | awk "/$section/,/END_$section/" |sed '1d;$d'
+}
 
 print_horizontal_line () {
 	printf "${WHITE}=======================================================================================${NC}\n"
